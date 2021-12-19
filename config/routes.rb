e1fd53347c0e71f5a3c0a034ad63898e8e656251
d-sazanami <dstrsazanami@gmail.com>
2021-12-20 08:19:06 +0900

    理解度チェック

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'top/index'
+  resources :rooms
   resources :books
   get 'greetings/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
