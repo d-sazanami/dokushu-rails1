ed5c57c872569bfc8246f3251a53955a644675d7
d-sazanami <dstrsazanami@gmail.com>
2021-12-27 15:43:05 +0900

    練習問題１

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_27_062821) do
+ActiveRecord::Schema.define(version: 2021_12_27_064055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,18 @@ ActiveRecord::Schema.define(version: 2021_12_27_062821) do
     t.text "descriptionn"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "entries", force: :cascade do |t|
+    t.string "user_name"
+    t.string "user_email"
+    t.date "reserved_date"
+    t.float "usage_time"
+    t.integer "room_id"
+    t.integer "people"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_email"], name: "index_entries_on_user_email", unique: true
   end
 
   create_table "rooms", force: :cascade do |t|
