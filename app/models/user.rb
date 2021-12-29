6ba705e7b5574318283c5561e5616358acc3266d
d-sazanami <dstrsazanami@gmail.com>
2021-12-29 09:00:27 +0900

    6.1.9 Valiratorクラスで共通の独自ヘルパーを実装する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,4 @@
 class User < ApplicationRecord
-    validate :email_check
+    validates :email, email_address:true
 
-    private
-    def email_check
-        email_pattern = /\A[\w+\._]+@[a-z\d\._]+\.[a-z]+\z/
-        unless email_pattern.match(self.email)
-            errors.add(:email, "正しいメールアドレスを入力してください")
-        end
-    end
-    
 end
