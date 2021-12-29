ba52678d9ea3ba81cd0b51d23a449e898286f8d0
d-sazanami <dstrsazanami@gmail.com>
2021-12-29 16:21:17 +0900

    rooms.numberに独自バリデータ適応

--- app/models/room.rb
+++ app/models/room.rb
@@ -6,4 +6,13 @@ class Room < ApplicationRecord
   validates :place, inclusion: { in: %w(東京 大阪 福岡 札幌 仙台 名古屋 金沢)}
   validates :number, presence: true
   validates :number, numericality: { less_than_or_equal_to: 30, greater_than_or_equal_to: 5}
+  validate :number_check
+
+  private
+  def number_check
+    unless self.number % 5 == 0
+      errors.add(:number, "収容人数は5の倍数で指定してください。")
+    end
+  end
+
 end
