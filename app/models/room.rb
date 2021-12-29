1f34c13b4b1b8519a3b826123e3aa0f023a686b6
d-sazanami <dstrsazanami@gmail.com>
2021-12-29 16:15:22 +0900

    rooms.nameフォーマットエラーメッセージ変更

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum:30 }
-  validates :name, format: { with: /.+#\d{2}\z/}
+  validates :name, format: { with: /.+#\d{2}\z/, message: "会議室名が正しくありません" }
   validates :place, presence: true
   validates :place, inclusion: { in: %w(東京 大阪 福岡 札幌 仙台 名古屋 金沢)}
   validates :number, presence: true
