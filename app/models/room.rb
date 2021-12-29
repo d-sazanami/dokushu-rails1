28fd8be7d4792ae8aad3b579b2dd15af3b604a3d
d-sazanami <dstrsazanami@gmail.com>
2021-12-29 15:37:57 +0900

    rooms.name maxleength指定

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,6 @@
 class Room < ApplicationRecord
   validates :name, presence: true
+  validates :name, length: { maximum:30 }
   validates :place, presence: true
   validates :number, presence: true
 end
