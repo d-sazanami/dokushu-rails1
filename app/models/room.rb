e20e304cda1d81af77ca04a91f96399e9894f559
d-sazanami <dstrsazanami@gmail.com>
2021-12-29 15:50:12 +0900

    rooms.numberに範囲バリデーション追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,4 +4,5 @@ class Room < ApplicationRecord
   validates :place, presence: true
   validates :place, inclusion: { in: %w(東京 大阪 福岡 札幌 仙台 名古屋 金沢)}
   validates :number, presence: true
+  validates :number, numericality: { less_than_or_equal_to: 30, greater_than_or_equal_to: 5}
 end
