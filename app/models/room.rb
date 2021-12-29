8078a3729e5bfe903e9bdecfb26deae7a3884a20
d-sazanami <dstrsazanami@gmail.com>
2021-12-29 15:43:27 +0900

    rooms.placeにinclusion指定

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,5 +2,6 @@ class Room < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum:30 }
   validates :place, presence: true
+  validates :place, inclusion: { in: %w(東京 大阪 福岡 札幌 仙台 名古屋 金沢)}
   validates :number, presence: true
 end
