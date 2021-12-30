70192728640910ed98716ae050368093e84eb5f8
d-sazanami <dstrsazanami@gmail.com>
2021-12-30 16:11:28 +0900

    単一テーブル継承

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_30_023404) do
+ActiveRecord::Schema.define(version: 2021_12_30_064704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,13 @@ ActiveRecord::Schema.define(version: 2021_12_30_023404) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_members_on_project_id"
+  end
+
+  create_table "pets", force: :cascade do |t|
+    t.string "type"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
