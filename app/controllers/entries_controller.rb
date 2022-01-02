c650d78a4bfa3a48be5a60ab75f620c5191f8832
d-sazanami <dstrsazanami@gmail.com>
2022-01-02 11:22:39 +0900

    理解度チェック２

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,48 @@
+class EntrisController < ApplicationController
+  before_action :set_entry, only: %i[ destroy ]
+
+  # GET /entrys or /entries.json
+  def index
+    @entries = Entry.all
+  end
+
+  # GET /entries/new
+  def new
+    @entry = Entry.new
+  end
+
+  # POST /entries or /entries.json
+  def create
+    @entry = Entry.new(entry_params)
+
+    respond_to do |format|
+      if @entry.save
+        format.html { redirect_to @entry, notice: "Entry was successfully created." }
+        format.json { render :show, status: :created, location: @entry }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @entry.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /entries/1 or /entries/1.json
+  def destroy
+    @entry.destroy
+    respond_to do |format|
+      format.html { redirect_to entries_url, notice: "Entry was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_entry
+      @entry = Entry.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def entry_params
+      params.require(:entry).permit(:title, :descriptionn)
+    end
+end
