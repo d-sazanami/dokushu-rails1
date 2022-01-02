755608af1311fbf5306b910bea14871c7019aad5
d-sazanami <dstrsazanami@gmail.com>
2022-01-02 10:55:23 +0900

    理解度チェック１

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :acceptances, only: [:new, :create]
+  root to: 'top#index'
   get 'top/index'
   resources :rooms
   resources :books
   get 'greetings/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
