355c448aad5b1803b3e9c93fc3385d517d01a6b6
d-sazanami <dstrsazanami@gmail.com>
2022-01-22 09:16:43 +0900

    ユーザー・フォームとバリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  validates :email, email_address:true
+  validates :name, presence: true
+  validates :email, email_address:true, presence: true, uniqueness: true
+  validates :password, presence: true
   has_many :pictures, as: :imageable
 end
